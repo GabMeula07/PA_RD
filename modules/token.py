4112ffe565659f54b20d39e8eb42a25d37fb5ce5
GabMeula07 <gabrielqueirozmeula@gmail.com>
2024-08-13 19:03:34 -0300

    Adicionando recurso para o limite de req da API

--- modules/token.py
+++ modules/token.py
@@ -2,20 +2,19 @@ import json
 from http import HTTPStatus
 import requests
 
+
 class Token:
-    def __init__(
-        self, app_acess, refresh_token, client_id, client_secret
-    ) -> None:
+    def __init__(self, app_acess, refresh_token, client_id, client_secret) -> None:
         self.__app_access = app_acess
         self.__refresh_token = refresh_token
         self.__client_id = client_id
         self.__client_secret = client_secret
-        
+
     def make_request(self, url):
         headers = {
             "accept": "application/json",
-            "authorization": f"Bearer {self.get_app_access()}"
-            }
+            "authorization": f"Bearer {self.get_app_access()}",
+        }
 
         response = requests.get(url, headers=headers)
         try:
@@ -45,31 +44,27 @@ class Token:
         newAppAccess = data_response["access_token"]
         newRefreshToken = data_response["refresh_token"]
 
-        
         self.set_app_access(newAppAccess)
         self.set_refresh_token(newRefreshToken)
         self.update_env(newRefreshToken, newAppAccess)
 
         return "OK"
-    
+
     def get_new_token_with_code(self, code):
         url = "https://api.rd.services/auth/token?token_by=code"
 
         payload = {
             "client_id": self.__client_id,
             "client_secret": self.__client_secret,
-            "code": code
+            "code": code,
         }
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json"
-        }
+        headers = {"accept": "application/json", "content-type": "application/json"}
         response = requests.post(url, json=payload, headers=headers)
         data_response = json.loads(response.text)
-        
+
         newAppAccess = data_response["access_token"]
         newRefreshToken = data_response["refresh_token"]
-    
+
         self.set_app_access(newAppAccess)
         self.set_refresh_token(newRefreshToken)
         self.update_env(newRefreshToken, newAppAccess)
